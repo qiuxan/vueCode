7f8879826d3b654984df59e71a3df1c0a57b2d7e
qiuxan <qiuxan@gmail.com>
2019-01-15 10:32:15 +1100

    Component Communication

--- main.js
+++ main.js
@@ -1,72 +1,29 @@
-Vue.component('tabs',{
-    template:`
-        <div>
+Vue.component('coupon',{
+    template:'<input placeholder="Enter your code" @blur="onCouponApplied">',
+     methods:{
+        onCouponApplied(){
         
-            <div class="tabs">
-                  <ul>
-                    <li v-for="tab in tabs" :class="{'is-active':tab.isActive}">
-                        <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a>
-                    </li> 
-                  </ul>
-            </div>
-            <div class="tabs-details">
-                <slot></slot>
-            </div>
-        
-        </div>
-    `,
-    data(){
-    
-        return {tabs:[]};
+            //alert('hddhdh');
+            //
+            this.$emit('applied');
+      }
     },
-
- 
-
-    created(){
-        this.tabs=this.$children; 
-    },
-    methods:{
-        selectTab(selectedTab){
-            this.tabs.forEach(
-            tab=>{
-                tab.isActive=(tab.name==selectedTab.name);
-            });
-        }
-    }
-
-/*
-    mounted(){
-    
-        console.log(this.$children);
-    }
-      */
-});
-Vue.component('tab',{
-    template:`
-       <div v-show="isActive"><slot></slot></div>
-    `,
-    props:{
-        name:{required:true},
-        selected:{default:false},
-
-    },
-    computed:{
-        href(){
-            return '#'+this.name.toLowerCase().replace(/ /g, '-');
-        }
-    },
-
-    data(){
-    return { isActive:false};
-    },
-
-    mounted(){
-        this.isActive=this.selected;
-    }
 });
 
 var app= new Vue({
     el: '#root',
 
+    data: {
+        couponApplied:false
+    
+    },
+    methods:{
+        onCouponApplied(){
+        
+           // alert('this is applied');
+            this.couponApplied=true;
+        }
+    
+    }
                   
 });
