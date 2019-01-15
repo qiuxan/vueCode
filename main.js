d6fb32b5dff662e4a43059c483344e12ef51cd8d
qiuxan <qiuxan@gmail.com>
2019-01-15 12:37:50 +1100

    an alternative way to go

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-window.Event = new Vue()
+window.Event = {
+
+	vue: new Vue(),
+
+	listen: function(event, callback){
+		this.vue.$on(event, callback);
+	},
+
+	fire: function(event, data=null){
+		this.vue.$emit(event, data);
+	},
+};
 
 Vue.component('coupon', {
     template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
 
     methods: {
         onCouponApplied() {
-            Event.$emit('applied')
+            Event.fire('applied')
         }
     }
 })
@@ -18,6 +29,6 @@ new Vue({
     },
 
     created() {
-        Event.$on('applied',  () => alert('Handling it!'))
+        Event.listen('applied',  () => alert('Handling it!'))
     }
 })
